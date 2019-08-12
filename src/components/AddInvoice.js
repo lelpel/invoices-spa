b55b505ee0aed773be0849d05bb3c63521b73688
Mykhaylo Khimchenko <m.khm.ua@gmail.com>
2019-08-12 22:15:11 +0300

    added hooks: AddInvoices

--- src/components/AddInvoice.js
+++ src/components/AddInvoice.js
@@ -1,40 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Modal, ModalHeader } from "reactstrap";
 import AddInvoiceForm from "./AddInvoiceForm";
 import { Row } from "reactstrap";
-class AddInvoice extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
 
-    this.toggle = this.toggle.bind(this);
-  }
+export default function AddInvoice(props) {
+  const [modal, setModal] = useState(false);
 
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
+  const toggle = () => {
+    setModal(!modal);
+  };
 
-  render() {
-    return (
-      <Row className="justify-content-center mt-3 mb-3">
-        <Button color="info" onClick={this.toggle}>
-          {this.props.buttonLabel}
-        </Button>
-        <Modal
-          isOpen={this.state.modal}
-          toggle={this.toggle}
-          className={this.props.className}
-        >
-          <ModalHeader toggle={this.toggle}>Add invoice</ModalHeader>
-          <AddInvoiceForm onSubmit={this.props.onSubmit} />
-        </Modal>
-      </Row>
-    );
-  }
+  return (
+    <Row className="justify-content-center mt-3 mb-3">
+      <Button color="info" onClick={toggle}>
+        {props.buttonLabel}
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} className={props.className}>
+        <ModalHeader toggle={toggle}>Add invoice</ModalHeader>
+        <AddInvoiceForm onSubmit={props.onSubmit} />
+      </Modal>
+    </Row>
+  );
 }
-
-export default AddInvoice;
